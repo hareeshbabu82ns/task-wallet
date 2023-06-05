37366fb5cb50cfa16e8bec49685b177610e65be4
Krishna Gupta <belivethatkg@gmail.com>
2023-06-05 19:29:11 +0530

    first commit.
    Signed-off-by: Krishna Gupta <belivethatkg@gmail.com>

---
+++ server/scheduler.ts
@@ -0,0 +1,9 @@
+// const cron = require("cron");
+
+// Create a new cron job
+// const job = new cron.CronJob("0 8 * * *", () => {
+//   console.log("HELLO");
+// });
+
+// // Start the cron job
+// job.start();
